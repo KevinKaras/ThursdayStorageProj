25d28a774153c3eb0d27a038c7ac890d73a66924
KevinKaras <kevinkaras2@gmail.com>
2021-01-07 13:16:44 -0600

    phase 1 done, lunchbreak

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,59 @@
+
 window.addEventListener("DOMContentLoaded", (event) => {
+  
+  let storeItem = () =>{
+  let buttonId = document.getElementById('add-to-cart');              // grabs the button, check on button a click
+  // console.log(buttonId);
+  
+  buttonId.addEventListener('click', event =>{                        // waiting to happen, then doing something  
+    
+    let currentItem = document.getElementById('items').value;         // we're storing the item that the user selected
+    let enteredQuantity = document.getElementById("quantity").value;  // we're storing the quantity that the user input
+    
+    localStorage.setItem(currentItem, enteredQuantity)                // setting in localstorage, the item the user selected as a key, the quanity they input
 
-  // const showCart = () => {
+    // console.log(currentItem.value);
 
-  // };
+    event.preventDefault();                                           // when the button is clicked, prevents everything from refreshing/losing data
+  })
 
-  // const storeItem = () => {
+  }
+  storeItem();                                                         // invoking the function we just made
 
-  // };
 
-  // const removeItem = () => {
+let cart = document.getElementById('shopping-cart');
 
-  // };
+let local = localStorage;
+
+// console.log(local);
+
+for (let key in local){
+  let quantity = localStorage.getItem(key);
+  
+  let food = key;
+
+  cart.appendChild()
+
+
+
+  // console.log(food);
+  // console.log(quantity);
+}
+
+
+
+
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
