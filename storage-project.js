faacd5a869f9271dae12c180eedd926675e81952
Mimi Ke <mimi.g.ke@gmail.com>
2021-01-07 15:24:16 -0600

    Second commit

--- storage-project.js
+++ storage-project.js
@@ -1,44 +1,68 @@
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  
+    let currentItem = document.getElementById('items').value;         // we're storing the item that the user selected
+    let enteredQuantity = document.getElementById("quantity").value;  // we're storing the quantity that the user input
+
+    localStorage.setItem(currentItem, enteredQuantity)                // setting in localstorage, the item the user selected as a key, the quanity they input
   let storeItem = () =>{
   let buttonId = document.getElementById('add-to-cart');              // grabs the button, check on button a click
   // console.log(buttonId);
-  
-  buttonId.addEventListener('click', event =>{                        // waiting to happen, then doing something  
-    
-    let currentItem = document.getElementById('items').value;         // we're storing the item that the user selected
-    let enteredQuantity = document.getElementById("quantity").value;  // we're storing the quantity that the user input
-    
-    localStorage.setItem(currentItem, enteredQuantity)                // setting in localstorage, the item the user selected as a key, the quanity they input
+
+  buttonId.addEventListener('click', event =>{
+    event.preventDefault();
+    let cart = document.getElementById('shopping-cart');
+
+    let local = localStorage; // object
+
+    console.log(local);
+
+    for (let key in local){
+      //let value = local[key]
+      //if (local[key]!== null){
+       if(localStorage.getItem(key) !== null){
+        console.log(key)
+        let quantity = local.getItem(key);
+
+
+        let food = key;
+        let cartItems = document.createElement("ci")
+        //console.log(cartItems)
+        cartItems.innerText = `${food}: ${quantity}\n`
+
+
+        cart.appendChild(cartItems)
+       }
+
+
+      }
+
 
     // console.log(currentItem.value);
-
-    event.preventDefault();                                           // when the button is clicked, prevents everything from refreshing/losing data
-  })
-
-  }
-  storeItem();                                                         // invoking the function we just made
-
-
-let cart = document.getElementById('shopping-cart');
-
-let local = localStorage;
+    ; // object
 
 // console.log(local);
 
-for (let key in local){
-  let quantity = localStorage.getItem(key);
-  
-  let food = key;
+// for (let key in local){
+//   //let value = local[key]
+//   //if (local[key]!== null){
+//    if(localStorage.getItem(key) !== null){
+//     console.log(key)
+//     let quantity = local.getItem(key);
+//     let food = key;
+//     let cart = document.getElementById('shopping-cart');
+// let local = localStorage
+//     let cartItems = document.createElement("ci")
+//     //console.log(cartItems)
+//     cartItems.innerText = `${food}: ${quantity}\n`
+  //   cart.appendChild(cartItems)
+  //  }
+  // }
+  //event.preventDefault();                                           // when the button is clicked, prevents everything from refreshing/losing data
 
-  cart.appendChild()
-
-
-
-  // console.log(food);
-  // console.log(quantity);
-}
+})
+storeItem();  
+  }
+                                                         // invoking the function we just made
 
 
 
@@ -46,14 +70,3 @@ for (let key in local){
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
