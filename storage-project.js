e34870fbb37a34425a96beb2781ecb72de1fd0c5
KevinKaras <kevinkaras2@gmail.com>
2021-01-07 16:29:46 -0600

    added removal buttons, working on event listener now

--- storage-project.js
+++ storage-project.js
@@ -1,68 +1,62 @@
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    let currentItem = document.getElementById('items').value;         // we're storing the item that the user selected
-    let enteredQuantity = document.getElementById("quantity").value;  // we're storing the quantity that the user input
-
-    localStorage.setItem(currentItem, enteredQuantity)                // setting in localstorage, the item the user selected as a key, the quanity they input
+  // let currentItem = document.getElementById('items').value;         // we're storing the item that the user selected
+  // let enteredQuantity = document.getElementById("quantity").value;  // we're storing the quantity that the user input
+  // localStorage.setItem(currentItem, enteredQuantity)                // setting in localstorage, the item the user selected as a key, the quanity they input
+  
+  
   let storeItem = () =>{
   let buttonId = document.getElementById('add-to-cart');              // grabs the button, check on button a click
-  // console.log(buttonId);
 
   buttonId.addEventListener('click', event =>{
+    let currentItem = document.getElementById('items').value;
+    let enteredQuantity = document.getElementById("quantity").value;
+    localStorage.setItem(currentItem, enteredQuantity) 
     event.preventDefault();
-    let cart = document.getElementById('shopping-cart');
 
-    let local = localStorage; // object
-
-    console.log(local);
-
-    for (let key in local){
-      //let value = local[key]
-      //if (local[key]!== null){
-       if(localStorage.getItem(key) !== null){
-        console.log(key)
-        let quantity = local.getItem(key);
-
-
-        let food = key;
-        let cartItems = document.createElement("ci")
-        //console.log(cartItems)
-        cartItems.innerText = `${food}: ${quantity}\n`
-
-
-        cart.appendChild(cartItems)
-       }
-
-
-      }
-
-
-    // console.log(currentItem.value);
-    ; // object
-
-// console.log(local);
-
-// for (let key in local){
-//   //let value = local[key]
-//   //if (local[key]!== null){
-//    if(localStorage.getItem(key) !== null){
-//     console.log(key)
-//     let quantity = local.getItem(key);
-//     let food = key;
-//     let cart = document.getElementById('shopping-cart');
-// let local = localStorage
-//     let cartItems = document.createElement("ci")
-//     //console.log(cartItems)
-//     cartItems.innerText = `${food}: ${quantity}\n`
-  //   cart.appendChild(cartItems)
-  //  }
-  // }
-  //event.preventDefault();                                           // when the button is clicked, prevents everything from refreshing/losing data
-
-})
-storeItem();  
+    storeCart()  // 12-15 adding item to local storage, everytime we update our cart with submit, were adding an item to local storage
+    }) 
   }
-                                                         // invoking the function we just made
+
+storeItem();
+  
+  let storeCart = () =>{
+    let cart = document.getElementById('shopping-cart');
+    let local = localStorage; 
+    cart.innerHTML = '<h2>Shopping Cart</h2>'
+    for (let i = 0; i < local.length; i++){
+      let key = local.key(i);
+      let quantity = local.getItem(key);                // getItem is like "getValue"                       
+      let cartItems = document.createElement("li");
+      cartItems.innerText = `${key}: ${quantity}`
+      let removeButton = document.createElement("button");
+      removeButton.setAttribute("class", "removal");
+      removeButton.innerText = "remove";
+      cartItems.appendChild(removeButton);
+  
+      cart.appendChild(cartItems)
+      
+    }
+}
+  storeCart();
+
+
+
+
+const removeItem = () =>{
+  let rButtons = document.querySelectorAll(".removal")
+  for (let i = 0; i < rButtons.length; i++){
+      let currentRButton = rButtons[i];
+      currentRButton.addEventListener('click', event =>{
+    
+      }
+    )
+  }  
+}
+
+
+
+
 
 
 
@@ -70,3 +64,8 @@ storeItem();
 
 
 });
+
+
+
+
+// get the div of 
